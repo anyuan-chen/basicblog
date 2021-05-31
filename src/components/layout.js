7778551ece6b16f275c997e6ac09811db083b0a9
Andrew Chen <andrew.chen.anyuan@gmail.com>
2021-05-31 18:02:04 -0400

    subpages

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,34 +1,37 @@
 import * as React from "react"
 import { Link } from "gatsby"
-import {list, nav} from "./layout.module.css"
+import { list, link } from "./layout.module.css"
 
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
   const isRootPath = location.pathname === rootPath
-  let header
-
-  if (isRootPath) {
-    header = (
-      <div>
-        <h1 className="main-heading">
-          <Link to="/">{title}</Link>
-        </h1>
-        <nav className = {nav}>
-          <ul className={list}>
-            <li>one</li>
-            <li>two</li>
-            <li>three</li>
-          </ul>
-        </nav>
-      </div>
-    )
-  } else {
-    header = (
-      <Link className="header-link-home" to="/">
-        {title}
-      </Link>
-    )
-  }
+  let header = (header = (
+    <div>
+      <h1 className="main-heading">
+        <Link to="/">{title}</Link>
+      </h1>
+      <nav>
+        <ul className={list}>
+          <li>
+            <Link to="/" className={link}>
+              {" "}
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/about" className={link}>
+              About
+            </Link>
+          </li>
+          <li>
+            <Link to="/extras" className={link}>
+              Extras
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  ))
 
   return (
     <div className="global-wrapper" data-is-root-path={isRootPath}>
--- src/pages/extras.js
+++ src/pages/extras.js
@@ -5,17 +5,17 @@ import Bio from "../components/bio"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const AboutMe = ({ data, location }) => {
+const Extras = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata?.title || `Title`
   return (
     <Layout location={location} title={siteTitle}>
-      <Seo title="About Me" />
-      <Bio></Bio>
+      <Seo title="Extras About Me" />
+      <h1>I will add stuff here</h1>
     </Layout>
   )
 }
 
-export default AboutMe
+export default Extras
 
 export const pageQuery = graphql`
   query {
