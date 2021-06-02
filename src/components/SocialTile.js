55b346548a641f42e06adf556c8ed5a8295ea996
Andrew Chen <andrew.chen.anyuan@gmail.com>
2021-06-02 09:49:04 -0400

    images and text added

--- src/components/SocialTile.js
+++ src/components/SocialTile.js
@@ -1,11 +1,41 @@
 import React from "react"
 import { StaticImage } from "gatsby-plugin-image"
+import {tile} from "./SocialTile.module.css"
 
 export default function SocialTile(props) {
-  return (
-    <div style={{ backgroundColor: props.color }}>
-      <StaticImage src={props.src} height="50" width="50"></StaticImage>
-      <h3>{props.text}</h3>
-    </div>
-  )
+  if (props.src.includes("binarysearch")) {
+    return (
+      <div className={tile}>
+        <StaticImage
+          src="../images/binarysearchlogo.png"
+          width="50"
+        ></StaticImage>
+        <h3>{props.text}</h3>
+      </div>
+    )
+  } else if (props.src.includes("atcoder")) {
+    return (
+      <div className={tile}>
+        <StaticImage src="../images/atcoderlogo.png" width="50"></StaticImage>
+        <h3>{props.text}</h3>
+      </div>
+    )
+  } else if (props.src.includes("codeforces")) {
+    return (
+      <div className={tile}>
+        <StaticImage
+          src="../images/codeforceslogo.png"
+          width="50"
+        ></StaticImage>
+        <h3>{props.text}</h3>
+      </div>
+    )
+  } else if (props.src.includes("dmoj")) {
+    return (
+      <div className={tile}>
+        <StaticImage src="../images/dmojlogo.png" width="50"></StaticImage>
+        <h3>{props.text}</h3>
+      </div>
+    )
+  }
 }
--- src/components/SocialTiles.js
+++ src/components/SocialTiles.js
@@ -9,6 +9,24 @@ export default function SocialTiles() {
       text: "Binary Search",
       color: "rgba(224,223,224,100%)",
     },
+    {
+      key: 2,
+      src: "../images/atcoderlogo.png",
+      text: "AtCoder",
+      color: "rgba(224,223,224,100%)",
+    },
+    {
+      key: 3,
+      src: "../images/codeforceslogo.png",
+      text: "CodeForces",
+      color: "rgba(224,223,224,100%)",
+    },
+    {
+      key: 4,
+      src: "../images/dmojlogo.png",
+      text: "DMOJ",
+      color: "rgba(224,223,224,100%)",
+    }
   ]
   let listTiles = properties.map((tileProperties) => 
     <SocialTile
